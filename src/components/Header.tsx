ba308d66b7f46f2160bff61666e0a71268255219
TheRegzi <reginekornbakk@gmail.com>
2025-02-17 13:28:25 +0100

    separating the layout components into their own files

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import Nav from "./Nav";
+
+function Header() {
+    return (
+      <header>
+        <div>Header</div>
+        <Nav />
+      </header>
+    );
+  }
+
+  export default Header;
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,42 +1,6 @@
-import { Link, Outlet } from "react-router-dom";
-
-
-function Nav() {
-  return (
-    <nav>
-      <ul>
-        <li>
-          <Link to="/">Home</Link>
-        </li>
-        <li>
-          <Link to="/products">Products</Link>
-        </li>
-        <li>
-            <Link to="/contact">Contact</Link>
-        </li>
-        <li>
-            <Link to="/cart">Cart</Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
-
-
-function Header() {
-  return (
-    <header>
-      <div>Header</div>
-      <Nav />
-    </header>
-  );
-}
-
-
-function Footer() {
-  return <footer>Footer</footer>;
-}
-
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import Footer from "./Footer";
 
 function Layout() {
   return (
