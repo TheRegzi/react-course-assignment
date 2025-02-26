d0acacb09caeffa429462f8dbfe845a93aed1596
TheRegzi <reginekornbakk@gmail.com>
2025-02-26 13:45:11 +0100

    feat: added more styling and media queries for cart

--- src/pages/Cart.styles.tsx
+++ src/pages/Cart.styles.tsx
@@ -8,6 +8,20 @@ margin: 2em auto;
 align-items: center;
 width: 800px;
 box-shadow: 0 5px 15px rgba(0,0,0,0.1);
+
+@media (max-width: 1100px) {
+    width: 500px;
+    
+  }
+
+  @media (max-width: 600px) {
+    width: 350px;
+    
+  }
+`
+
+export const Headline = styled.h1`
+font-family: ${props => props.theme.fonts.heading};
 `
 
 export const ProductLink = styled(Link)`
@@ -20,6 +34,11 @@ width: 150px;
 height: 210px;
 object-fit: cover;
 
+@media (max-width: 600px) {
+    width: 120px;
+    
+  }
+
 `
 
 export const ProductContainer = styled.div`
@@ -31,11 +50,44 @@ border: 2px solid #EBDBFA;
 border-radius: 10px;
 margin: 1em;
 overflow: hidden;
+
+@media (max-width: 1100px) {
+    width: 400px;
+  }
+
+@media (max-width: 600px) {
+    width: 300px;
+    height: 180px;
+  }
+
 `
 
 export const ProductInfo = styled.div`
-padding: 0em 1em 1em;
+padding: 0em 2em 1em;
 
+@media (max-width: 600px) {
+    padding: 0 0.5em;
+  }
+
+`
+
+export const ProductTitle = styled.h2`
+font-family: ${props => props.theme.fonts.heading};
+
+@media (max-width: 600px) {
+    margin: 0.5em 0;
+  }
+`
+
+export const ProductDescription = styled.p`
+@media (max-width: 1100px) {
+    display: none;
+  }
+`
+
+export const ProductPrice = styled.div`
+font-weight: bold;
+font-size: 1.1em;
 `
 
 export const CheckoutButton = styled(Link)`
@@ -46,13 +98,12 @@ cursor: pointer;
 padding: 0.5em 1em;
 font-family: ${props => props.theme.fonts.secondary};
 font-size: 1.1em;
-margin: 1em 0em 2em;
+margin: 1em 0em 2.5em;
 
 &:hover {
     transform: scale(1.03);
     opacity: 0.8;
   }
-
 `
 
 export const QuantityContainer = styled.div`
@@ -73,10 +124,15 @@ cursor: pointer;
 width: 40px;
 height: 40px;
 
-
 &:hover {
     transform: scale(1.03);
     opacity: 0.8;
   }
+`
 
+export const TotalInfo = styled.div`
+font-weight: bold;
+font-size: 1.3em;
+margin: 0.5em;
+font-family: ${props => props.theme.fonts.primary};
 `
